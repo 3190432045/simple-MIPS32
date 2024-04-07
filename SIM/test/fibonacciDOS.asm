cb3d2e0bcc0ccda1197e19e0ed9fbf9f732738b4
JingYuan Wu <jw26u22@soton.ac.uk>
2024-04-07 10:39:32 +0100

    2024/04/07 周日

--- SIM/test/fibonacciDOS.asm
+++ SIM/test/fibonacciDOS.asm
@@ -11,44 +11,52 @@ _fibonacci PROC NEAR
 	mov	ebp, esp
 	sub	esp, 12					; 0000000cH
 
-; 15   :     int first = 0, second = 1, next = 0;
+; 15   :     register int first = 0, second = 1, next = 0;
 
 	mov	DWORD PTR _first$[ebp], 0
 	mov	DWORD PTR _second$[ebp], 1
 	mov	DWORD PTR _next$[ebp], 0
-$L36:
+$loop_body$35:
 
 ; 16   :
 ; 17   :
-; 18   :     for (; next != 28657;)
+; 18   : loop_body:
+; 19   :     if( next == 28657)
 
 	cmp	DWORD PTR _next$[ebp], 28657		; 00006ff1H
-	je	SHORT $L37
+	jne	SHORT $L36
 
-; 20   :         next = first + second;
+; 20   :         goto loop_end;
+
+	jmp	SHORT $loop_end$37
+$L36:
+
+; 21   :     next = first + second;
 
 	mov	eax, DWORD PTR _first$[ebp]
 	add	eax, DWORD PTR _second$[ebp]
 	mov	DWORD PTR _next$[ebp], eax
 
-; 21   :         first = second;
+; 22   :     first = second;
 
 	mov	ecx, DWORD PTR _second$[ebp]
 	mov	DWORD PTR _first$[ebp], ecx
 
-; 22   :         second = next;
+; 23   :     second = next;
 
 	mov	edx, DWORD PTR _next$[ebp]
 	mov	DWORD PTR _second$[ebp], edx
 
-; 23   :
-; 24   : //        printf("%d\n", next);
-; 25   :     }
+; 24   : //    printf("%d\n", next);
+; 25   :     goto loop_body;
 
-	jmp	SHORT $L36
-$L37:
+	jmp	SHORT $loop_body$35
+$loop_end$37:
 
-; 26   : }
+; 26   :
+; 27   : loop_end:
+; 28   :     return;
+; 29   : }
 
 	mov	esp, ebp
 	pop	ebp
